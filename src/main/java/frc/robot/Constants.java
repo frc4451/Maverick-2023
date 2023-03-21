df6894b2efe1cac72d30db333f1e99bb736fe2e5
Lewis Arias <lewisgarias@outlook.com>
2023-03-21 19:14:50 -0400

    Limelight aiming but with a PID Controller (untested).

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,7 +46,9 @@ public class Constants {
     // Settings for arcade velocity drive
     public static class DT_Settings {
         public static final double TARGETING_TURNING_PG = 0.3; // Proportional gain for limelight targeting
-        public static final double TARGETING_TURNING_SG = 0; // Frixion constant for limelight targeting
+        public static final double TARGETING_TURNING_IG = 0.0; // Integral gain for limelight targeting
+        public static final double TARGETING_TURNING_DG = 0.0; // Derivative gain for limelight targeting
+        public static final double TARGETING_TURNING_SG = 0.0; // Frixion constant for limelight targeting
 
         public static final double TURN_SENSITIVITY = 0.65; // Joystick turn scaling factor for Curve Drive
         public static final double MIN_TURN = 0.2;
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.Constants;
@@ -7,8 +8,17 @@ import frc.robot.Constants;
 public class Limelight {
     private final NetworkTable table;
 
+    private final PIDController TURN_CONTROLLER = new PIDController(
+            Constants.DT_Settings.TARGETING_TURNING_PG,
+            Constants.DT_Settings.TARGETING_TURNING_IG,
+            Constants.DT_Settings.TARGETING_TURNING_DG);
+
     public Limelight(String tableKey) {
         table = NetworkTableInstance.getDefault().getTable(tableKey);
+
+        // We always want these settings for TURN_CONTROLLER
+        this.TURN_CONTROLLER.setSetpoint(0);
+        this.TURN_CONTROLLER.setTolerance(1);
     }
 
     // Epic boilerplate per:
@@ -31,24 +41,23 @@ public class Limelight {
      * @return Vertical Offset From Crosshair To Target (-20.5 to 20.5 degrees)
      */
     // public double getYOffset() {
-    // return this.table.getEntry("ty").getDouble(0.0);
+    //     return this.table.getEntry("ty").getDouble(0.0);
     // }
 
     // We can make these constants later if needed.
     public void setAimingPipelineEnabled(boolean isEnabled) {
         if (isEnabled) {
-            table.getEntry("pipeline").setNumber(1);
+            this.table.getEntry("pipeline").setNumber(1);
         } else {
-            table.getEntry("pipeline").setNumber(0);
+            this.table.getEntry("pipeline").setNumber(0);
         }
     }
 
     public double getTurnFromLimelight() {
         if (this.hasTargets()) {
-            return this.getXOffset()
-                    * Constants.DT_Settings.TARGETING_TURNING_PG
-                    + Constants.DT_Settings.TARGETING_TURNING_SG;
-        } else
+            return this.TURN_CONTROLLER.calculate(this.getXOffset()) * Constants.DT_Settings.MAX_VELOCITY;
+        } else {
             return 0;
+        }
     }
 }
