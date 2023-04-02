67c7e2698d94dfee504de0d0ec798da82d7dc83a
Lewis Arias <lewisgarias@outlook.com>
2023-04-02 14:33:34 -0400

    Comment Photoner's getTurnTo a bit more and simplify its logic slightly.

--- src/main/java/frc/robot/subsystems/Photoner.java
+++ src/main/java/frc/robot/subsystems/Photoner.java
@@ -30,18 +30,24 @@ public class Photoner {
     /**
      * Output for turning to best target with offset
      * 
-     * @param tagOffsetMeters Offset from AprilTag in meters
+     * @param horizonalOffsetMeters Horizontal offset from AprilTag in meters.
+     *                              The exact point this is offset from depends on
+     *                              your PhotonVision configuration in the UI.
      */
-    public double getTurnTo(double tagOffsetMeters) {
+    public double getTurnTo(double horizonalOffsetMeters) {
         final PhotonTrackedTarget target = this.result.getBestTarget();
+
+        // If we don't have this null check the code'll crash when there isn't a target
         if (target == null) {
             return 0;
         }
 
         final Transform3d targetPose = target.getBestCameraToTarget();
 
-        double error = -(targetPose.getY() + tagOffsetMeters);
-        if (this.result.hasTargets() && (Math.abs(error) >= 0.1)) {
+        final double error = -(targetPose.getY() + horizonalOffsetMeters);
+        // We shouldn't need to to check if there's any targets here because if there
+        // weren't targets we would've early returned.
+        if (Math.abs(error) >= 0.1) {
             return (error * Constants.DT_Settings.TARGETING_TURNING_PG)
                     + (Math.signum(error) * Constants.DT_Settings.TARGETING_TURNING_SG);
         } else {
